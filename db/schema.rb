0efbcbfb9dd54038cfd711a6144d3241bddd770e
Umang Patel <umangp90@gmail.com>
2017-04-30 17:40:02 -0400

    switched db to use postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170430212115) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
